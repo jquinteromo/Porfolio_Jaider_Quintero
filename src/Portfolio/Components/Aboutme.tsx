0e9836591a67eb12be9e0363ecadbc0d1b22e967
jquinteromo <qjaider1111@gmail.com>
2025-05-31 13:35:22 -0500

    Justity text Aboutme

--- src/Portfolio/Components/Aboutme.tsx
+++ src/Portfolio/Components/Aboutme.tsx
@@ -3,7 +3,7 @@ export default function Aboutme() {
     <div>
       <section className="px-6 md:px-20 py-10">
         <h2 className="text-2xl font-semibold text-sky-400 mb-4">Sobre mí</h2>
-        <p className="text-slate-300 max-w-2xl">
+        <p className="text-justify  text-slate-300 max-w-2xl">
           Soy aprendiz del SENA en etapa productiva, con enfoque en el
           desarrollo web moderno. Me gusta construir sitios desde cero, pensando
           tanto en cómo funcionan internamente como en su diseño y experiencia
